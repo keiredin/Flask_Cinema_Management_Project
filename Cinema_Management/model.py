b33e1ecbb7b820c932e7c8b885b2d7b96c4e8462
MebeaN <60043138+MebeaN@users.noreply.github.com>
2021-05-22 12:37:27 +0300

    base template first touch

--- Cinema_Management/model.py
+++ Cinema_Management/model.py
@@ -1,3 +1,15 @@
 from datetime import datetime
+from enum import unique
 from Cinema_Management import db,login_manager,bcrypt
 from flask_login import UserMixin
+
+class Client(db.Model):
+    __table__name = "Clients"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f"Client('{self.username}', '{self.email}')"
