fb409312714c90a54e9f01d1ba76611ea0e4f5a4
MebeaN <60043138+MebeaN@users.noreply.github.com>
2021-05-26 14:42:49 +0300

    404 and users db table added

--- Cinema_Management/model.py
+++ Cinema_Management/model.py
@@ -5,12 +5,19 @@ from flask_login import UserMixin
 
 
 class Client(db.Model):
-    __table__name = "Clients"
+    __tablename__ = "client"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
+    username = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(30), nullable=False, unique=True)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(100), default='user.png')
 
     def __repr__(self):
-        return f"Client('{self.username}', '{self.email}')"
+        return f"Client('{self.username}', '{self.email}', '{self.image}')"
+
+# class Movie(db.Model):
+#     __tablename__ = "movies"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(50), nullable=False)
